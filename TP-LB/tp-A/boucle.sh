9e8e28fcacb3e9678397e0c31a657901293b4f3c
Waped <aliounethiaw100@gmail.com>
2024-10-15 15:17:31 +0200

    Modifications qui seront validées : 	modifié :         tp-A/boucle.sh 	modifié :         tp-A/default.conf 	nouveau fichier : tp-B/app1/Dockerfile 	nouveau fichier : tp-B/app1/app.py 	nouveau fichier : tp-B/app1/requirements.txt 	nouveau fichier : tp-B/app2/Dockerfile 	nouveau fichier : tp-B/app2/app.py 	nouveau fichier : tp-B/app2/requirements.txt#	nouveau fichier : tp-B/nginx/Dockerfile 	nouveau fichier : tp-B/nginx/docker-compos.yl 	nouveau fichier : tp-B/nginx/nginx.con supprimé :        ../TP9/ClientUDP.class 	supprimé :        ../TP9/MonProg.class 	supprimé :        ../TP9/ServeurUDP.class

--- TP-LB/tp-A/boucle.sh
+++ TP-LB/tp-A/boucle.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-for ((i=0 ; i<500;i++))
-do
-    resp = $(curl localhost:83)
-    if [$resp = "Hello 1"];
-    then
-    ((c+=1))
-    fi
-
-done
-
-echo $c
+curl localhost:83?[1-500] | grep "Hello 1" | wc -l 2> /dev/null
